5a53e760cb3e7990a9dc599c29586094f34f1024
dr-george-dev <labegeorgeedu@gmail.com>
2025-10-19 08:49:13 +0100

    This is where my comments started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// fixed the error coming from .inter by adding this line - dr-george
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "unipod Innovation Hub Training Management System",
+  title: "Unipod Innovation Hub Training Management System",
   description: "Next.js School Management System",
 };
 
+//Displays on all pages - by dr-george
 export default function RootLayout({
   children,
 }: Readonly<{
